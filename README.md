06ae04fc814c58db5eeca82ce10dd2654ed8d8d7
Ryan C C <98823018+ryanrenda@users.noreply.github.com>
2022-03-29 03:28:48 +0000

    graphed the sequence and told the user the max

--- README.md
+++ README.md
@@ -8,18 +8,18 @@
 _Replace anything surrounded by the `< >` symbols._
 
 ## SUMMARY:
- < Provide a short description of your program's functionality. No more than a couple sentences. >
+ it draws a graph of the 3np1 sequence and tells the user the max point
 
 ## GRACE DAYS
-Grace days used for this assignment: < # >
+Grace days used for this assignment: < 0 >
 
-Grace days remaining: < # >/5
+Grace days remaining: < 5 >/5
 
 ## KNOWN BUGS AND INCOMPLETE PARTS:
- < What parts of the project you were not able to complete >
+ N/A
 
 ## REFERENCES:
- < List any outside resources used >
+ N/A
 
 ## MISCELLANEOUS COMMENTS:
- < Anything you would like the grader to know >
+ N/A
--- main.py
+++ main.py
@@ -1,18 +1,50 @@
-
+import turtle
 def seq3np1(n):
     """
         Print the 3n+1 sequence from n, terminating when it reaches 1.
         args: n (int) starting value for 3n+1 sequence
         return: None
     """
+    count = 0
     while(n != 1):
-        print(n)
         if(n % 2) == 0:        # n is even
             n = n // 2
         else:                 # n is odd
             n = n * 3 + 1
-    print(n)                  # the last print is 1
+        count = count + 1
+    print("The count is: " + str(count))
+    return count
+
+def graphing(n):
+  window = turtle.Screen()
+  draw = turtle.Turtle()
+  max_data = turtle.Turtle()
+  max_so_far = 0
+  draw.speed(0)
+  max_data.speed(0)
+  window.setworldcoordinates(0, 0, 10, 10)
+  iteration = 0
+  for i in range(1, n+1):
+    result = seq3np1(i)
+    if (result > max_so_far):
+      max_so_far = result
+      iteration = i
+      max_data.clear()
+    window.setworldcoordinates(0,0,i+10,max_so_far+10)
+    draw.goto(i, result)
+    data = "maximum so far: iteration: " +str(iteration) + " result " + str(max_so_far)
+    max_data.up()
+    max_data.goto(0, max_so_far)
+    max_data.down()
+    max_data.write(data)
+  window.exitonclick()
 
 def main():
-	seq3np1(3)
+  upper_bound = int(input("Enter a postitive value for the upper bound of the range: "))
+  if (upper_bound < 0):
+    exit()
+  for start in range(upper_bound):
+    print(f"Current value of start is {start+1}")
+    seq3np1(start+1)
+  graphing(100)
 main()
